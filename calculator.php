fe0d62ec6fbb6374cfe9844350b045feb7501709
cs143 <frankchen1698@gmail.com>
2015-10-01 01:59:23 -0700

    get rid of space showing up successfully

--- calculator.php
+++ calculator.php
@@ -1,16 +1,23 @@
 <html>
 <body>
 
+<form action = "calculator.php" method = "get">
+<input type='text' name = 'expr'>
+<input type = 'submit' value = 'Calculate'>
+</form>
+
 <?php
 
 function removeExtraSpaces($input) {
    for($i = 0; $i < strlen($input); $i++) {
        if($input[$i] == ' ' && $input[$i + 1]) {   //Detected extra spaces
            for($j = $i; $j < strlen($input); $j++) {
-	       $input[$i] = $input[$i + 1];  //Move everything back 1 character
-	       }       
+	       $input[$j] = $input[$j + 1];  //Move everything back 1 character
+	         
    	   }
+       }
    }
+   return $input;
 }
 
 function errorChecker($input) {
@@ -35,6 +42,7 @@ function performCalculation($input) {
     if(!errorChecker($performOperation)) {
         return;
     }
+    return $performOperation;
 
     //Perform multiplication & division
     for($i = 0; $i < strlen($performOperation); $i++) {
@@ -50,20 +58,9 @@ function performCalculation($input) {
 }
 
 $expr = $_GET["expr"];
-//echo "$expr";
-echo $_GET["expr"];
+$temp = removeExtraSpaces($expr);
 
-?>
-
-<form action = "calculator.php" method = "get">
-<input type='text' name = 'expr'>
-<input type = 'submit' value = 'Calculate'>
-</form>
-
-<?php
-
-$expr = $_GET["expr"];
-echo $expr;
+echo $temp;
 
 ?>
 

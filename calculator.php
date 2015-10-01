0029feab6616ed0d421a6b6222fdc7d491689562
Matthew Allen Lin <Darthpwner@s-169-232-70-118.resnet.ucla.edu>
2015-09-30 23:11:44 -0700

    Added changes to calculator

--- calculator.php
+++ calculator.php
@@ -1,18 +1,42 @@
-function removeSpaces($input) {
+function removeExtraSpaces($input) {
    for($i = 0; $i < strlen($input); $i++) {
-       if($input[$i] == ' ') {	    //If you detect a space
+       if($input[$i] == ' ' && $input[$i + 1]) {   //Detected extra spaces
            for($j = $i; $j < strlen($input); $j++) {
 	       $input[$i] = $input[$i + 1];  //Move everything back 1 character
-	   }
-       }
+	   }       
    }
 }
 
 function errorChecker($input) {
     $pattern = '[0-9-+/*.]';   //Regex to detect legal characters
     
-
     for($i = 0; $i < strlen($input); $i++) {
-
+        if(!preg_match($pattern, $input[$i]) {
+	   echo "Invalid Expression!";
+	   return false;
+	} 
     }
+    return true;	    
+}
+
+function performCalculation($input) {
+    //Remove unnecessary spaces
+    $cleanedInput = removeExtraSpaces($input);
+
+    //Perform error checking
+    if(!errorChecker($cleanedInput)) {
+        return;
+    }
+
+    //Perform multiplication & division
+    for($i = 0; $i < strlen($cleanedInput); $i++) {
+        //TODO
+    }
+
+    //Perform addition & subtraction
+    for($i = 0; $i < strlen($cleanedInput); $i++) {
+        //TODO
+    }
+
+    return $cleanedInput;
 }

61ea8808394779bd07a5cf121ebd60fc1ef40591
Matthew Allen Lin <Darthpwner@s-169-232-70-39.resnet.ucla.edu>
2015-10-07 20:26:50 -0700

    Ready to submit :)

--- calculator.php
+++ calculator.php
@@ -17,77 +17,74 @@ Type an expression in the following box (e.g., 10.5+20*3/25).
 <?php
 
 function errorChecker($input) {
+  $pattern0 = "/\-\ *\-\ *\-/"; //Handles more than 3 minus signs
+  if (preg_match($pattern0, $input)) {
+     echo "Invalid Expression: more than 3 minus signs";
+     return;
+  }
+
    // replace -- with +
    $input = str_replace("--", "+", $input);
- 
   
-    $pattern0 = "/[^\d\+\-\*\/\.\ ]/"; 
-   // removing unwanted characters
-   if(preg_match($pattern0, $input)) {
+   $pattern1 = "/[^\d\+\-\*\/\.\ ]/"; // removing unwanted characters
+   if(preg_match($pattern1, $input)) {
        //echo $input;
        echo "Invalid Expression!";
        return;
    }
 
-  $pattern70_1 = "/^\ *[\+\-\*\/]\ *$/"; //Prevent single operators
-  if(preg_match($pattern70_1, $input)) {
+  $pattern2 = "/^\ *[\+\-\*\/]\ *$/"; //Prevent single operators
+  if(preg_match($pattern2, $input)) {
       echo "Invalid Expression: Single operator";
       return;
   }
 
-  $pattern71 = "/[\+\-\*\/]\ *[\+\*\/]/"; //Handles strange combinations of operators
-  if(preg_match($pattern71, $input)) {
+  $pattern3 = "/[\+\-\*\/]\ *[\+\*\/]/"; //Handles strange combinations of operators
+  if(preg_match($pattern3, $input)) {
       echo "Invalid Expression: Strange combinations of operators";
       return;
   }
 
-//BUG
-  $pattern69 = "/-\ *-\ *-/"; //Handles more than 3 minus signs
-  if (preg_match($pattern69, $input)) {
-     echo "Invalid Expression: more than 3 minus signs";
-     return;
-  }
-
-   $pattern1 = "/^\ *[\+\*\/]\ *\d/";
-   if(preg_match($pattern1, $input)) {
+   $pattern4 = "/^\ *[\+\*\/]\ *\d/";
+   if(preg_match($pattern4, $input)) {
        echo "Invalid Expression: Handling operator at beginning";
        return;
    }
 
-   $pattern2 = "/\d\ *[\+\-\*\/]\ *$/"; //Handles hanging operators at the end
-   if (preg_match($pattern2, $input)) {
+   $pattern5 = "/\d\ *[\+\-\*\/]\ *$/"; //Handles hanging operators at the end
+   if (preg_match($pattern5, $input)) {
        echo "Invalid Expression: hanging operator at end";
        return;
    }
 
-   $pattern3 = "/[\+\*\/]\ *[\+\*\/]/"; //Handles multiple operators without operands
-  if (preg_match($pattern3, $input)) {
+   $pattern6 = "/[\+\*\/]\ *[\+\*\/]/"; //Handles multiple operators without operands
+  if (preg_match($pattern6, $input)) {
       echo "Invalid Expression: multiple operators without operands";
       return;
   }
 
-  $pattern4_1 = "/0\./"; //Allows 0. 
-  $pattern4 = "/\b0\d/"; //Handling leading 0s
-  if (preg_match($pattern4_1, $input)) {
+  $pattern7_1 = "/0\./"; //Allows 0. 
+  $pattern7 = "/\b0\d/"; //Handling leading 0s
+  if (preg_match($pattern7_1, $input)) {
       //Do nothing
-  } else if (preg_match($pattern4, $input)){ // removing leading zeros
+  } else if (preg_match($pattern7, $input)){ // removing leading zeros
        echo "Invalid Expression: leading zero";
        return;
   }
 
-  $pattern5 = "/\d\.\d*\./"; //Handling multiple decimal points
+  $pattern8 = "/\d\.\d*\./"; //Handling multiple decimal points
   // removing multiple decimal points
-  if(preg_match($pattern5, $input)) {
+  if(preg_match($pattern8, $input)) {
      echo "Invalid Expression: multiple decimal points";
      return;                                                  
   }
 
-  $pattern6_1 = "/\d\ *\/\ *0.?0*[1-9+]/"; //Allows 0/[1-9+] division
-  $pattern6 = "/\d\ *\/\ *0/"; //Handling dividing by 0
+  $pattern9_1 = "/\d\ *\/\ *0.?0*[1-9+]/"; //Allows 0/[1-9+] division
+  $pattern9 = "/\d\ *\/\ *0/"; //Handling dividing by 0
 
-  if (preg_match($pattern6_1, $input)) {
+  if (preg_match($pattern9_1, $input)) {
       //Do nothing
-  } else if (preg_match($pattern6, $input)){ // removing dividing by 0
+  } else if (preg_match($pattern9, $input)){ // removing dividing by 0
        //echo $input;
        echo "Division by zero error!";
        return;

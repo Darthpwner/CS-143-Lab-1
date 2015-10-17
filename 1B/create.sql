ebba65b46e571545bbde3f6e363b82d693959c6f
cs143 <frankchen1698@gmail.com>
2015-10-17 01:10:18 -0700

    added initial code to create.sql

---
+++ 1B/create.sql
@@ -0,0 +1,39 @@
+# Movie Table; primary key: id
+CREATE TABLE Movie (
+
+) ENGINE = InnoDB;
+
+# Actor Table; primary key: id
+CREATE TABLE Actor (
+
+) ENGINE = InnoDB;
+
+#Director Table; 
+CREATE TABLE Director (
+
+) ENGINE = InnoDB;
+
+#MovieGenres;
+CREATE TABLE MovieGenre (
+
+) ENGINE = InnoDB;
+
+CREATE TABLE MovieDirector (
+
+) ENGINE = InnoDB;
+
+CREATE TABLE MovieActor (
+
+) ENGINE = InnoDB;
+
+CREATE TABLE Review (
+
+) ENGINE = InnoDB;
+
+CREATE TABLE MaxPersonID (
+
+);
+
+CREATE TABLE MaxMovieID (
+	
+);
--- 1B/queries.sql
+++ 1B/queries.sql
@@ -1,4 +1,4 @@
-/*Give me the names of all the actors in the movie 'Die Another Day'.
+Give me the names of all the actors in the movie 'Die Another Day'.
  Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).*/
 
 SELECT DISTINCT last, first
@@ -7,6 +7,12 @@ FROM ACTOR A, MOVIEACTOR MA, MOVIE M
 WHERE M.title = 'Die Another Day' AND A.id = MA.aid AND M.id = MA.mid
 
 /*Give me the count of all the actors who acted in multiple movies.*/
+/*SELECT COUNT(*)
+FROM (
+	SELECT Actor.id
+	FROM Actor A, MovieActor MA
+	WHERE A.id = MA.aid
 
+)*/
 
-/*To Be Determined by the user*/
+/*To Be Determined by the user

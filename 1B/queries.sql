7675977ff65bb4b1c2af52d3fe9b2bdb2c6b83c8
Matthew Allen Lin <Darthpwner@s-169-232-70-47.resnet.ucla.edu>
2015-10-17 00:49:42 -0700

    Push changes with Frank, see if it works for Die Another Day

--- 1B/queries.sql
+++ 1B/queries.sql
@@ -1,9 +1,10 @@
 /*Give me the names of all the actors in the movie 'Die Another Day'.
  Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).*/
 
-SELECT firstname, lastname
-FROM actor, movie
-WHERE title = "Die Another Day"
+SELECT DISTINCT last, first
+#SELECT firstname, lastname
+FROM ACTOR A, MOVIE M, MOVIEACTOR MA
+WHERE M.title = 'Die Another Day' AND A.id = MA.aid AND M.id = MA.mid
 /*Give me the count of all the actors who acted in multiple movies.*/
 
 

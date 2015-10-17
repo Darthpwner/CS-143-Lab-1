af89cc849dfed5bcc07607949ce7efcb7a534918
cs143 <frankchen1698@gmail.com>
2015-10-17 11:40:09 -0700

    added stuff to queries.sql

--- 1B/queries.sql
+++ 1B/queries.sql
@@ -1,9 +1,9 @@
-Give me the names of all the actors in the movie 'Die Another Day'.
+/*Give me the names of all the actors in the movie 'Die Another Day'.
  Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).*/
 
 SELECT DISTINCT last, first
 #SELECT firstname, lastname
-FROM ACTOR A, MOVIEACTOR MA, MOVIE M
+FROM Actor A, MovieActor MA, Movie M
 WHERE M.title = 'Die Another Day' AND A.id = MA.aid AND M.id = MA.mid
 
 /*Give me the count of all the actors who acted in multiple movies.*/

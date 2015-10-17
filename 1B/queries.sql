c8dae3acc7903350a3a92e4dbc7c51c2dc9faa81
Matthew Allen Lin <Darthpwner@s-169-232-70-47.resnet.ucla.edu>
2015-10-17 00:32:03 -0700

    Added in comments about the problems, starting now

---
+++ 1B/queries.sql
@@ -0,0 +1,6 @@
+/*Give me the names of all the actors in the movie 'Die Another Day'.
+ Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).*/
+
+/*Give me the count of all the actors who acted in multiple movies.*/
+
+/*To Be Determined by the user*/

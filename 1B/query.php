11cc4280c0babe7aa48f01ce65548569dc45e9b3
cs143 <frankchen1698@gmail.com>
2015-10-16 22:59:23 -0700

    added query.php

---
+++ 1B/query.php
@@ -0,0 +1,25 @@
+<html>
+<head> <title> Project 1B: Query </title></head>
+  
+<body>
+
+	<h1> Calculator </h1>
+	By Kang (Frank) Chen & Matthew Lin <br/>
+	Type an SQL query in the following box:
+
+	<p>
+		<form action="." method="GET">
+		<textarea name="query" cols="60" rows="8">
+		</textarea><br />
+		<input type="submit" value="Submit" />
+		</form>
+	</p>
+
+<?php
+
+
+?>
+
+
+</body>
+</html>

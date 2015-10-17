c0ae8afb9cfe38c6e787bc684c029897b67d88ad
cs143 <frankchen1698@gmail.com>
2015-10-17 01:14:22 -0700

    added movie create table

--- 1B/create.sql
+++ 1B/create.sql
@@ -1,6 +1,12 @@
 # Movie Table; primary key: id
 CREATE TABLE Movie (
-
+	id INTEGER NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	year INTEGER NOT NULL,
+	rating VARCHAR(10),
+	company VARCHAR(10) NOT NULL,
+	PRIMARY KEY (id),
+	CHECK (id > 0 AND id <= MaxMovieID(id))
 ) ENGINE = InnoDB;
 
 # Actor Table; primary key: id
@@ -31,9 +37,9 @@ CREATE TABLE Review (
 ) ENGINE = InnoDB;
 
 CREATE TABLE MaxPersonID (
-
+	id INT DEFAULT 69000
 );
 
 CREATE TABLE MaxMovieID (
-	
+	id INT DEFAULT 4750
 );

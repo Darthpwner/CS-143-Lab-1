0bb3c28551efaa8212bb6bde1f5c95c7c414566e
cs143 <frankchen1698@gmail.com>
2015-10-18 17:41:20 -0700

    made more modifications to query.php

--- 1B/query.php
+++ 1B/query.php
@@ -1,9 +1,11 @@
 <html>
-<head> <title> Project 1B: Query </title></head>
+<head> 
+	<title> Project 1B: Query </title>
+</head>
   
 <body>
 
-	<h1> CS143: Project 1B - Query </h1>
+	<h1> CS143: Project 1B - Querying </h1>
 	By Kang (Frank) Chen & Matthew Lin <br/>
 	Type an SQL query in the following box:
 
@@ -22,20 +24,22 @@ if ($_GET["query"]){
 
 	// cs 143 connection
 	$db_connection = mysql_connect("localhost", "cs143", "");
+	// check for no connection
 	if (!$db_connection) {
 		$error_msg = mysql_error($db_connection);
 		print "Connection cannot be established: $error_msg <br />";
 		exit(1);
 	}
 
-	// obtain the input
+	// get input and select database
 	$query = $input;
 	mysql_select_db("CS143", $db_connection);
 
 	// display user's query
-	echo "Your query: ".$query." <br />";
+	echo "<b>Your query:</b> ".$query." <br />";
 	echo "<h3> Results from MySQL: </h3>";
 
+	// get the result from using mysql_query 
 	$result = mysql_query($query, $db_connection);
 
 	// check that the query is valid
@@ -43,9 +47,10 @@ if ($_GET["query"]){
 		die('The query could not be performed <br/>' . mysql_error());
 	}
 
-	// get the results and place into table
+	// get the results and place into tables to be displayed later
 	$k = 0;
 	echo '<table border=1 cellspacing=1 cellpadding=2><tr>';
+
 	while ($k < mysql_num_fields($result)){
 		$field = mysql_fetch_field($result, $k);
 		echo '<td><b>' . $field->name . '</b></td>';
@@ -67,13 +72,13 @@ if ($_GET["query"]){
 		}
 		echo '</td><tr>';
 	}
+	// close tr and table tag
 	echo '</tr></table>';
 
-	// close the database, for now we are done
+	// close the database
 	mysql_close($db_connection);
 }
 ?>
-
 
 </body>
 </html>

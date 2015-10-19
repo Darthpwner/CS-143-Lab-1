4b14caf134a72464138cac9fa94b9008dad22688
Matthew Allen Lin <Darthpwner@s-169-232-70-19.resnet.ucla.edu>
2015-10-18 17:22:27 -0700

    Commented out correctly, dont use --

--- 1B/queries.sql
+++ 1B/queries.sql
@@ -1,12 +1,12 @@
---Give me the names of all the actors in the movie 'Die Another Day'.
--- Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).
+/*Give me the names of all the actors in the movie 'Die Another Day'.
+Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space). */
 
 /*SELECT DISTINCT first, last
 FROM Actor A, MovieActor MA, Movie M
 WHERE M.title = 'Die Another Day' AND A.id = MA.aid AND M.id = MA.mid
 */
 
---Give me the count of all the actors who acted in multiple movies.*/
+/*Give me the count of all the actors who acted in multiple movies.*/
 
 /*SELECT COUNT(*)
 FROM (
@@ -17,8 +17,8 @@ FROM (
 	HAVING COUNT(mid) > 1
 ) as tmp*/
 
---Give me the titles of all the Romance movies
+/*Give me the titles of all the Romance movies*/
 SELECT title
 FROM Movie M, MovieGenre MG
-WHERE M.id = MG.mid AND MG.genre = 'Romance'
+WHERE M.id = MG.mid AND MG.genre = 'Romance';
 

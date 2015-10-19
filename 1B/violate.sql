e76030bcbec8372daf355c5b94b968bbff228f2e
Matthew Allen Lin <Darthpwner@s-169-232-70-19.resnet.ucla.edu>
2015-10-18 17:50:14 -0700

    Set up violations.sql

---
+++ 1B/violate.sql
@@ -0,0 +1,19 @@
+#Movie Violations
+ALTER TABLE Movie ADD PRIMARY KEY (id); #
+CHECK (id > 0 AND id <= MaxMovieID(id))
+
+#Actor Violations
+
+#Director Violations
+
+#MovieGenres Violations
+
+#MovieDirector Violations
+
+#MovieActor Violations
+
+#Review Violations
+
+#MaxPersonID Violations
+
+#MaxMovieID Violations

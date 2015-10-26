3b4ca8f0ef9820a213b90188b1d22ee3f0ede590
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-10-26 08:03:08 -0700

    Made chnages and defined preliminary interface for each file

---
+++ 1C/addMovieActor.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Project 1C: Add Movie/Actor relationship</title>
+	</head>
+
+	<body>
+		<h1>CS 143: Project 1C - Add Movie/Actor Relation</h1>
+		Add new actor in a movie:
+		<br></br>
+		<form action="./addAMovieActor.php" method="GET>
+			"Movie: "
+			// Need to fix this part, add the database
+			"Actor: "
+			// Need to fix this part, add the database
+			"Role: "
+			<input type="text" name="role" maxlength="50">
+			<br>
+			<input type="submit" value="Add it!">
+		</form>
+		<hr> #Used as the separator
+	</body>
+</html>
---
+++ 1C/search.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Project 1C: Search Actor/Movie</title>
+	</head>
+
+	<body>
+	</body>
+</html>

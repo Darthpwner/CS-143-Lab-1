3a64cb217091232c4e2911c84d9a82137977b39d
Matthew Allen Lin <Darthpwner@s-169-232-70-163.resnet.ucla.edu>
2015-10-26 00:43:38 -0700

    Fixing up addActorDirector

---
+++ 1C/addActorDirector.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<title>Project 1C: Add Actor Director</title>
+	</head>
+
+	<body>
+		<h1> CS 143: Project 1C - Add Actor/Director</h1>
+		By Kang (Frank) Chen & Matthew Lin <br/>
+
+		Add new actor/director:
+		<br></br>
+		<form method="GET" action="./addActorDirector.php"
+			Identity: 
+			<input type="radio" checked="true" value="Actor" name="identity"></input>
+			Actor
+			<input type="radio" value="Director" name="identity"></input>
+			Director
+			<br></br>
+			<hr></hr>
+			First name:
+			<input type="text" maxlength="20" name="first"></input>
+			<br></br>
+			Last name: 
+			<input type="text" maxlength="20" name="last"></input>
+			<br></br>
+			Sex:
+			<input type="radio" checked="true" value="Male"></input>
+			Male
+			<input type="radio" value="Female" name="sex"></input>
+			Female
+			<br></br>
+			Date of Birth:
+			<input type="radio" value="Female" name="sex"></input>
+			Date of Death:
+			<input type="text" name="dod"></input>
+			(leave blank if alive now)
+			<br></br>
+			<input type = "submit"
+		</form>
+	</body>
+</html>

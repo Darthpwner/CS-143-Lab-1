99de229469a1a96587e189b1a292bbe2283ac326
Matthew Allen Lin <Darthpwner@s-169-232-70-128.resnet.ucla.edu>
2015-10-26 22:28:23 -0700

    Implemented UI for the search feature

--- 1C/search.php
+++ 1C/search.php
@@ -5,5 +5,12 @@
 	</head>
 
 	<body>
+		Search for actors/movies
+		<form action="./search.php" method="GET">
+			Search: 
+			<input type="text" name="keyword">
+			<input type="submit" value="Search">
+		</form>
+		<hr>
 	</body>
 </html>

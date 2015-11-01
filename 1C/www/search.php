99210c168db6e21b1e4d41ae308d5c2d8f2d0cc1
Matthew Allen Lin <Darthpwner@s-169-232-70-12.resnet.ucla.edu>
2015-10-31 22:24:40 -0700

    Trying things with PHP parameters

--- 1C/www/search.php
+++ 1C/www/search.php
@@ -73,7 +73,7 @@ if ($_GET["keyword"]){
     	$first = $row[0];
     	$last = $row[1];
     	$dob = $row[2];
-    	print "Actor: <a href=showActorInfo.php/>$first $last($dob)</a><br />";
+    	print "Actor: <a href=showActorInfo.php?actor=$result/>$first $last($dob)</a><br />";
 	}
 
 	newLine();
@@ -95,7 +95,7 @@ if ($_GET["keyword"]){
 	while($row = mysql_fetch_row($result)) {
     	$title = $row[0];
     	$year = $row[1];
-    	print "Movie: <a href=showMovieInfo.php/>$title($year)</a><br />";
+    	print "Movie: <a href=showMovieInfo.php?movie=$result/>$title($year)</a><br />";
 	}
 
 	// close the database

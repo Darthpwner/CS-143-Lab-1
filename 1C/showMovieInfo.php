2fa08e95f5fada408fd77c41fc06e4f2e8daffd6
Matthew Allen Lin <Darthpwner@s-169-232-70-12.resnet.ucla.edu>
2015-10-31 20:43:58 -0700

    Called shit for showActorInfo and showMovieInfo

--- 1C/showMovieInfo.php
+++ 1C/showMovieInfo.php
@@ -2,7 +2,16 @@
 	<head>
 		<title>Project 1C: Show Movie Information</title>
 	</head>
-
 	<body>
+		<?php
+			function yolo() {
+				echo "Bastille";
+			}
+
+			yolo();
+
+
+			include 'search.php';
+		?>
 	</body>
 </html>

2b5f2218db4b32817d8c3e1b3523346419799d01
Matthew Allen Lin <Darthpwner@s-164-67-124-50.resnet.ucla.edu>
2015-11-01 00:18:16 -0700

    What the fuck is wrong with my syntax

--- 1C/www/showActorInfo.php
+++ 1C/www/showActorInfo.php
@@ -9,19 +9,16 @@
 			$input = $_GET["aid"];   // WHAT VALUE DO I have?
 			echo "$input<br />";
 
-			$actor_query = "SELECT first, last, dob FROM Actor WHERE id=$input";
+			$actor_query = "SELECT first, last, sex, dob FROM Actor WHERE id=$input";
 
 			$result = mysql_query($actor_query, $db_connection);
 
 			//Assign variables
-			$row = mysql_fetch_row($actor_query);
-			echo "$row[0]";
-			echo $row[1];
-
-
+			// $row = mysql_fetch_row($result);
+			// echo "$row[0]";
+			// echo "$row[1]";
 
     		//$name = ".$row[0]." ".$row[1].";
-    		echo "<b>Sex:</b> ".$row[2]."<br/>"; //sex
     		// $sex = $row[2];
     		// $dob = $row[3];
     		// $dod = $row[4];
